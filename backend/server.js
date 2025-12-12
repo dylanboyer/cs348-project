f8af596eaf84403933e71a0646b64e209fbec78d
Dylan Boyer <boyer51@purdue.edu>
2025-12-11 19:46:35 -0500

    changed mongodb password and moved to .env

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const MONGO_URI = "mongodb+srv://dylan:informal1@taskr.gwizy5a.mongodb.net/?appName=taskr";
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
